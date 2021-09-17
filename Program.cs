75c8d929bad6756d7af799927974300b52d7abd4
Hampus Andersson <hampe.01@hotmail.com>
2021-09-17 15:26:26 +0200

    Added array & methods

--- Program.cs
+++ Program.cs
@@ -1,67 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace Adressbok{
     class Program{
+        
+        
+        static void addName(ref string tmp,ref string[] adress){
+            while (true){
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Enter full name: ");
+                tmp = Console.ReadLine();
+                Console.ResetColor();
+                if (tmp.Contains(" ") && tmp.Length > 3){
+                    List<string> list = new List<string>(adress.ToList());
+                    list.Add(tmp);
+                    adress = list.ToArray();
+                    break;
+                }
+                else{
+                    Console.WriteLine("Please enter a both first and surname.");
+                }
+            }
+        }
+        static void showNames(ref string[] adress){
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Names:");
+            Console.ResetColor();
+            if(adress != null){
+                foreach(string name in adress){
+                    Console.WriteLine(name);
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
         static void Main(string[] args){
             char choice;
-            string adress = "";
+            string[] adress = {};
             string tmp = "";
-            int names = 0;
             while (true){
                 Console.Clear();
                 Console.WriteLine("1. Add name");
                 Console.WriteLine("2. Show all names");
-                Console.WriteLine("3. Clear names"); 
-                Console.WriteLine("4. Statistics");
+                Console.WriteLine("3. Clear names");
                 Console.WriteLine("Q. Quit");
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("Enter choice (1-4): ");
+                Console.Write("Enter choice (1-3): ");
                 choice = Console.ReadKey().KeyChar;
                 Console.ResetColor();
                 Console.WriteLine("\n");
                 
                 switch (choice){
                     case '1':
-                        while (true){
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.Write("Enter full name: ");
-                            tmp = Console.ReadLine();
-                            Console.ResetColor();
-                            if (tmp.Contains(" ") && tmp.Length > 3){
-                                adress += tmp + "\n";
-                                names++;
-                                break;
-                            }
-                            else{
-                                Console.WriteLine("Please enter a both first and surname.");
-                            }
-                        }
+                        addName(ref tmp,ref adress);
                         break;
 
                     case '2':
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Names:");
-                        Console.ResetColor();
-                        Console.Write(adress);
-                        Console.WriteLine("Press any key to continue...");
-                        Console.ReadKey();
+                        showNames(ref adress);
                         break;
 
                     case '3':
-                        Console.WriteLine("");
-                        adress = "";
-                        names = 0;
+                        adress = null;
+ 
                         break;
-
-                    case '4':
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Statistics:");
-                        Console.ResetColor();
-                        Console.WriteLine("Amount of names: " + Convert.ToString(names));
-                        Console.WriteLine("Amount of Characters: " + Convert.ToString(adress.Length - names));
-                        Console.WriteLine("Press any key to continue...");
-                        Console.ReadKey();
-                        break;
-
                     case 'q' or 'Q':
                         Console.ResetColor();
                         return; 
