c762bd011113c34a07e4849929a5c718721da91c
Hampus Andersson <hampe.01@hotmail.com>
2021-09-22 09:31:13 +0200

    Cloned from github classroom

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Adressbok{
                 Console.ResetColor();
                 if (!String.IsNullOrEmpty(tmp))
                 {
-                    adress.Add(tmp);
+                    adress.Add(tmp.ToLower());
                     break;
                 }            
                 else{
@@ -32,39 +32,136 @@ namespace Adressbok{
             }
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+            
         }
         
         static void clearNames(ref List<string> adress){
+            adress.Clear();
+            Console.ResetColor();
+        }
+
+        static void search(ref List<string> adress){
+            string searchStr = "";
+            while (true){
+                writeColor($"Search: {searchStr}",ConsoleColor.Blue);
+                var key = System.Console.ReadKey(true);
+                Console.ResetColor();
+                if (key.Key == ConsoleKey.Enter){
+                    break;
+                }
+                Console.Clear();
+                searchStr += key.KeyChar;
+                
+                foreach(string name in adress){
+                    if (name.Contains(searchStr.ToLower())){
+                        Console.WriteLine(name);
+                    }
+                }
+            }
 
         }
+        static void remove(ref List<string> adress){
+            string searchStr = "";
+            while (true){
+                writeColor($"Press enter to remove: {searchStr}",ConsoleColor.Blue);
+                var key = System.Console.ReadKey(true);
+                Console.ResetColor();
+                if (key.Key == ConsoleKey.Enter){
+                    Console.Clear();
+                    foreach(string name in adress.ToList()){
+                        if (name.Contains(searchStr.ToLower())){
+                            adress.RemoveAt(adress.IndexOf(name));
+                        }
+                    }
+                    break;
+                }
+                Console.Clear();
+                searchStr += key.KeyChar;
+                foreach(string name in adress){
+                    if (name.Contains(searchStr.ToLower())){
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+
+        }
+        static void isActive(string str,int thisChoice,ref int currentChoice){
+            if (thisChoice == currentChoice)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(str);
+                Console.ResetColor();
+            }else{
+                Console.WriteLine(str);
+            }
+        }
+        static void changeUp(ref int currentChoice){
+            if (currentChoice > 1)
+            {
+                currentChoice--;
+            }
+        }
+        static void changeDown(ref int currentChoice){
+            if (currentChoice < 6)
+            {
+                currentChoice++;
+            }
+        }
         static void Main(string[] args){
-            char choice;
+            int choice;
             List<string> adress = new List<string>();
             string tmp = "";
+            int currentChoice = 1;
             while (true){
-                Console.Clear();
-                Console.WriteLine("1. Add name");
-                Console.WriteLine("2. Show all names");
-                Console.WriteLine("3. Clear names");
-                Console.WriteLine("Q. Quit");
-                writeColor("Enter choice (1-3): ",ConsoleColor.Blue);
-                choice = Console.ReadKey().KeyChar;
-                Console.ResetColor();
+                while (true)
+                {
+                    Console.Clear();
+                    writeColor("Choose using arrow keys (Press enter to continue)\n",ConsoleColor.Blue);
+                    Console.ResetColor();
+                    isActive("Add name",1,ref currentChoice);
+                    isActive("Show all names",2,ref currentChoice);
+                    isActive("Clear names",3,ref currentChoice);
+                    isActive("Search",4,ref currentChoice);
+                    isActive("Remove name(s)",5,ref currentChoice);
+                    isActive("Quit",6,ref currentChoice);
+                    
+                    var key = System.Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        choice = currentChoice;
+                        break;
+                    }else if( key.Key == ConsoleKey.DownArrow){
+                        changeDown(ref currentChoice);
+                    }else if(key.Key == ConsoleKey.UpArrow){
+                        changeUp(ref currentChoice);
+                    }
+                    
+                }
+                
                 Console.WriteLine("\n");
                 switch (choice){
-                    case '1':
+                    case 1:
                         addName(ref tmp,ref adress);
                         break;
 
-                    case '2':
+                    case 2:
                         showNames(ref adress);
                         break;
 
-                    case '3':
-                        adress.Clear();
+                    case 3:
+                        clearNames(ref adress);
  
                         break;
-                    case 'q' or 'Q':
+                    case 4:
+                        search(ref adress);
+ 
+                        break;
+                    case 5:
+                        remove(ref adress);
+ 
+                        break;
+                    case 6:
                         Console.ResetColor();
                         return; 
                     default: 
